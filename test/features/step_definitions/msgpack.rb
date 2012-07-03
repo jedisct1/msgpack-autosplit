ed8d7e535cc19a17483c5fb9ca4fb9a254837899
Frank Denis <github@pureftpd.org>
2012-07-03 16:51:02 -0700

    Add a definition for creating large msgpack files.

--- test/features/step_definitions/msgpack.rb
+++ test/features/step_definitions/msgpack.rb
@@ -4,8 +4,13 @@ Given /^a file named "(.*?)" with short msgpack data$/ do |filename|
   File.open(filename, 'w') { |f| f.write([1, 2, 3, 4].to_msgpack) }
 end
 
+Given /^a file named "(.*?)" with large msgpack data$/ do |filename|
+  File.open(filename, 'w') { |f| f.write([1, 2, 3, 4].to_msgpack * 100_000) }
+end
+
 Then /^every file for "(.*?)" should be a valid msgpack stream$/ do |glob|
   Dir[glob].each do |filename|
     MessagePack::unpack(File.read(filename))
   end
 end
+
